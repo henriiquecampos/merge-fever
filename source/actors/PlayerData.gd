da458db2e1747449f4c76119a9d326208312d2bc
Tumeo <github@tumeo.space>
2019-10-06 18:14:11 -0300

    Add a simple "tag" support (#1)
    * Add a simple "tag" support
    
    * Update version variable name

--- source/actors/PlayerData.gd
+++ source/actors/PlayerData.gd
@@ -4,9 +4,15 @@ signal updated
 signal lose
 
 const MAX_MISSED_RELEASES = 3
+const MIN_MAJOR_BUMP = 5
 var potential_score = 0 setget set_potential_score
 var score = 0 setget set_score
 var missed_releases = 0 setget set_missed_releases
+var current_version = [0, 0]
+var commit_counters = {
+	'fix': 0,
+	'feature': 0,
+}
 
 func set_potential_score(new_value):
 	potential_score = new_value
@@ -31,9 +37,26 @@ func check_missed_releases():
 	if missed_releases >= MAX_MISSED_RELEASES:
 		emit_signal("lose")
 
+func bump_version():
+	if commit_counters['feature'] >= MIN_MAJOR_BUMP:
+		current_version = [current_version[0]+1, 0, 0]
+	elif commit_counters['feature'] > 0:
+		current_version = [current_version[0], current_version[1]+1, 0]
+	elif commit_counters['fix'] > 0: # fix == 0: possible "missed release" condition?
+		current_version[2] += 1
+	commit_counters = {
+		'fix': 0,
+		'feature': 0,
+	}
+	emit_signal("updated")
 
 func reset():
 	missed_releases = 0
 	potential_score = 0
 	score = 0
+	current_version = [0, 0, 0]
+	commit_counters = {
+		'fix': 0,
+		'feature': 0,
+	}
 	emit_signal("updated")
--- source/interfaces/GitHistory.gd
+++ source/interfaces/GitHistory.gd
@@ -4,8 +4,10 @@ export (PackedScene) var git_command = preload("res://interfaces/GitCommand.tscn
 
 func add_git_command(text):
 	if get_child_count() > 4:
-		var first = get_child(0)
-		first.queue_free()
+		for child in get_children():
+			if not child.is_queued_for_deletion():
+				child.queue_free()
+				break
 	var instance = git_command.instance()
 	instance.text = text
 	add_child(instance)
@@ -13,11 +15,16 @@ func add_git_command(text):
 
 func _on_player_head_area_entered(area):
 	if area.is_in_group("bug"):
+		PlayerData.commit_counters['fix'] += 1
 		add_git_command("git merge bug-fix")
 	elif area.is_in_group("feature"):
+		PlayerData.commit_counters['feature'] += 1
 		add_git_command('git commit -m "new feature!"')
 	elif area.is_in_group("release"):
 		add_git_command("git merge development")
+		PlayerData.bump_version()
+		var version = PlayerData.current_version
+		add_git_command('git tag "v%d.%d.%d"' % version)
 
 
 func _on_player_checked_out(message):
